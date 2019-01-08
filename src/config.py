c5fa7183973057cf6f0fb111ea5d8ca6b3919355
Tyrus <tyrus.miles@gmail.com>
2019-01-07 22:56:21 -0700

    where user information goes

---
+++ src/config.py
@@ -0,0 +1,10 @@
+# MAKE SURE TO PULL ALL INFORMATION INBETWEEN THE
+
+
+user = 'EMAIL_HERE' # username for golfwang.com
+password = 'PASSWORD_HERE' # password for golfwang.com
+
+cred_num = 'CREDIT_CARD_NUMBER_HERE' # The card number which should be on the front of the card
+cred_name = 'NAME_ON_CARD' # The first and last name on the card
+cred_exp = 'EXP_DATE' #no slashes or spaces (ex. '1219')
+cred_cvv = 'CCV_HERE' # 3 digit security code on the back of card
